9106ec385c498c91267d0be59392c0e421c2dd26
Kalpanamurugesan <83176115+Kalpanamurugesan@users.noreply.github.com>
2023-09-03 22:38:14 +0530

    Update and rename Facebook12.java to Whatsapp.java

--- Whatsapp.java
+++ Whatsapp.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Facebook12 {
+public class Whatsapp {
 
     public static void main(String[] args) {
         // Set the path to your Chrome WebDriver executable
